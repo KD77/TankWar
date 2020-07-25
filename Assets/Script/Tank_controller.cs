73b7519621adaf9188299ede756761fd2c23204e
KD77 <kalid.diriye@gmail.com>
2020-07-25 23:44:33 +0200

    first commit

---
+++ Assets/Script/Tank_controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankDemo {
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Tank_inputs))]
+public class Tank_controller: MonoBehaviour
+{
+        public float tankSpeed = 15f;
+        public float tankRotationSpeed = 20f;
+
+
+        private Rigidbody rb;
+        private Tank_inputs inputs;
+    // Start is called before the first frame update
+    void Start()
+    {
+            rb = GetComponent<Rigidbody>();
+            inputs = GetComponent<Tank_inputs>();
+
+    }
+
+    // Update is called once per frame
+    void fixedUpdate()
+    {
+        if (rb && inputs)
+            {
+                HandelMovement();
+
+
+            }
+    }
+     protected virtual void HandelMovement()
+     { // move tank forword
+      Vector3 wantedPosition = transform.position + (transform.forward * tankSpeed * Time.deltaTime);
+            rb.MovePosition(wantedPosition);
+     }
+    }
+}
---
+++ Assets/Script/Tank_inputs.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TankDemo
+{
+    public class Tank_inputs : MonoBehaviour
+    {
+        [Header("Input Properties")]
+        public Camera camera;
+
+        // properties
+        private Vector3 reticlePositon;
+        public Vector3 ReticlePosition
+        {
+            get { return reticlePositon; }
+
+        }
+
+        private Vector3 reticleNormal;
+        public Vector3 ReticleNormal
+        {
+            get { return reticleNormal;  }
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            if (camera)
+            {
+                HandelInputs();
+
+            }
+
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(reticlePositon, 0.5f);
+            
+        }
+        protected virtual void HandelInputs()
+        {
+            Ray screenRay = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if( Physics.Raycast(screenRay,out hit))
+            {
+                reticlePositon = hit.point;
+                reticleNormal = hit.normal;
+            }
+        }
+    }
+}
